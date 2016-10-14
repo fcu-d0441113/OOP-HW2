812972d5d6c67d8e3a7457dfa9c6aceaeeee02a6
Lin <Lin@FE05>
2016-10-14 09:44:34 +0800

    loop_HW

---
+++ src/fcu/iecs/oop/Continue.java
@@ -0,0 +1,18 @@
+package fcu.iecs.oop;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++){
+			
+			if(i==0||i==2||i==4)
+				continue;
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
