1d0b7e5004c487e2114553b87617f5775c683d07
Lin <Lin@FE05>
2016-10-14 09:43:03 +0800

    循環比較

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -8,17 +8,19 @@ public class StringComparator {
 		
 		String str1,str2;
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.printf("Enter a string 1:");
-		str1 = scanner.next();
-		System.out.printf("Enter a string 2:");
-		str2 = scanner.next();
-		
-		if(str1.equalsIgnoreCase(str2))
-			System.out.printf("The two strings are the same.");
-		else
-			System.out.printf("The two strings are not the same.");
-
+		while(true){
+			System.out.printf("Enter a string 1:");
+			str1 = scanner.next();
+			System.out.printf("Enter a string 2:");
+			str2 = scanner.next();
+			if(str1.equalsIgnoreCase(str2)){
+				System.out.printf("The two strings are the same.");
+				break;
+			}
+			else
+				System.out.printf("The two strings are not the same.\n");
+		}
+		scanner.close();
 	}
 
 }
